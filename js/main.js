9c54e272ab14aadd962f4c79b6684be856bb4adc
Dmitry Chekurkov <chekurkov@gmail.com>
2019-04-10 12:39:05 +0300

    Get API-news

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", init);
+function init(){
+    /* My Hurd Code then push into third branch and 
+    merge with DEV and create pretty UI */
+    var apikey = "b426b1761d53466c9ca16514d9a564d8";
+    var category = ["business", "entertainment", "health", "science", "technology"];
+
+    var callbackFunction = [
+        { news: businessNews },
+        { news: entertaimentNews },
+        { news: healthNews },
+        { news: scienceNews },
+        { news: technologyNews }
+      ];
+
+    for (var i = 0; i < category.length; i++) {
+        Request(category[i], apikey, callbackFunction[i].news);
+    }
+}
+
+function Request(category, apikey, callback){
+    var url = `https://newsapi.org/v2/top-headlines?country=ua&category=${category}&apiKey=${apikey}`;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url, true);
+  
+    xhr.send();
+  
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4) return;
+  
+      if (xhr.status != 200) {
+        var errStatus = xhr.status;
+        var errText = xhr.statusText;
+        console.log(errStatus + ": " + errText);
+      } else {
+        var data = JSON.parse(xhr.responseText);
+        callback(data);
+      }
+    };
+}
+
+function businessNews(news){
+    console.log("businessNews", news);
+}
+function entertaimentNews(news){
+    console.log("EntertaimentNews", news);
+}
+function healthNews(news){
+    console.log("HealthNews", news);
+}
+function scienceNews(news){
+    console.log("scienceNews", news);
+}
+function technologyNews(news){
+    console.log("technologyNews",news);
+}
